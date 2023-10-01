230303ee9b8d2b1ce54e9afd4b4af11ed736680a
Ragool krishnan <ragool.krishnan@notice.studio>
2023-10-01 22:25:54 +0200

    fix

--- it/src/i18n/translations/it.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  heading: "Crowdin testing",
-  question1: " What is Notice ?",
-  answer1: "Notice as the one place to write, manage, and publish all of your web content.First, it’s great to write in. You have a super straightforward editor - kinda like Notion’s - with a super versatile structure for blocks. You can create whatever content structure you want. Plus, you’ve got AI tools like GPT and DALL-E to assist you at all times right inside Notice.",
-  question2: "What are blocks?",
-  answer2: "Blocks are the core 💛 of Notice. They power everything we do! This article goes over both the high-level vision and the intricacy of what blocks are."
-};
